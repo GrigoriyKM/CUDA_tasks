f1e6744ba8ec94ad4b1cb10a26eaa08309ef5667
GrigoriyKM <konyukhovgm@gmail.com>
2024-09-28 14:39:24 +0700

    add calculating time and report

--- task1/sinf.cu
+++ task1/sinf.cu
@@ -8,7 +8,7 @@
 const int N = 1000000000; // 10^9
 
 // Ядро для инициализации массива
-__global__ void initializeArray(float *arr)
+__global__ void initializeArray(double *arr)
 {
     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < N)
@@ -17,7 +17,7 @@ __global__ void initializeArray(float *arr)
     }
 }
 
-double calcError(float *hostArr, int arraySize)
+double calcError(double *hostArr, int arraySize)
 {
     double err = 0;
     for (int i = 0; i < arraySize; i++)
@@ -38,39 +38,46 @@ void checkCudaError(cudaError_t status)
 
 int main()
 {
-    // Время начала выполнения
-    clock_t start = clock();
+
     int device = 1;
     cudaSetDevice(device);
     // Выделение памяти на GPU для массива
-    float *d_arr;
-    checkCudaError(cudaMalloc(&d_arr, N * sizeof(float)));
+    double *d_arr;
+    checkCudaError(cudaMalloc(&d_arr, N * sizeof(double)));
 
     dim3 blockSize = dim3(256);
     dim3 numBlocks = dim3((N + blockSize.x - 1) / blockSize.x);
+    // Время начала выполнения
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
     // Запуск ядра для инициализации массива
     initializeArray<<<numBlocks, blockSize>>>(d_arr);
 
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
     // Выделение памяти на CPU для массива
-    float *h_arr = (float *)malloc(N * sizeof(float));
+    double *h_arr = (double *)malloc(N * sizeof(double));
 
     // Копирование массива с GPU на CPU
-    checkCudaError(cudaMemcpy(h_arr, d_arr, N * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaError(cudaMemcpy(h_arr, d_arr, N * sizeof(double), cudaMemcpyDeviceToHost));
     checkCudaError(cudaDeviceSynchronize());
 
     // Расчет ошибки
     printf("Ошибка (sinf) = %0.10f \n", calcError(h_arr, N));
+
+    // Вывод времени выполнения
+    printf("Время выполнения: %f мсек \n", milliseconds);
 
     // Освобождение памяти на GPU
     cudaFree(d_arr);
 
     // Освобождение памяти на CPU
     free(h_arr);
-
-    // Время окончания выполнения
-    clock_t end = clock();
-
-    // Вывод времени выполнения
-    printf("Время выполнения: %0.5f секунд \n", (end - start) / CLOCKS_PER_SEC);
     return 0;
 };
